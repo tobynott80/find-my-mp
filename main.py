546a6ce82086d264dc78e8f8111adca78f66c741
Toby Nott <NottTJ@cardiff.ac.uk>
2022-01-19 18:30:57 +0000

    added id return function

--- main.py
+++ main.py
@@ -1,23 +1,40 @@
 import requests
 
 
-search = str(input("Enter PostCode or other search term: "))
-url = "https://members-api.parliament.uk/api/Location/Constituency/Search?searchText="+ search
 
+#if __name__ == "__main__":
+if False:
+    url = "https://members-api.parliament.uk/api/Location/Constituency/Search?searchText="+ search
+    search = str(input("Enter PostCode or other search term: "))
+    resp = requests.get(url)
 
-resp = requests.get(url)
+    print("Searching")
 
-print("Searching")
+    if resp.status_code == 200: 
+        print("Done!")
+        data = resp.json()
+        if data["totalResults"] == 0:
+            print("No results found")
+        print(data["resultContext"])
+        firstRes = (data["items"][0]["value"])
+        mp = firstRes["currentRepresentation"]["member"]["value"]
+        print("Your constituancy is {}".format(firstRes["name"]))
+        print("Your MP is {}".format(mp["nameDisplayAs"]))
+        print("Your MP belongs to the {} party".format(mp["latestParty"]["name"]))
 
-if resp.status_code == 200:
-    print("Done!")
+def searchMP(query): #searches for mp and returns ID
+    url = "https://members-api.parliament.uk/api/Location/Constituency/Search?searchText="+ query
+    resp = requests.get(url)
     data = resp.json()
-    print(data["resultContext"])
-    firstRes = (data["items"][0]["value"])
-    mp = firstRes["currentRepresentation"]["member"]["value"]
-    print("Your constituancy is {}".format(firstRes["name"]))
-    print("Your MP is {}".format(mp["nameDisplayAs"]))
-    print("Your MP belongs to the {} party".format(mp["latestParty"]["name"]))
-
-
+    if resp.status_code == 200:
+        if data["totalResults"] == 0:
+            return "Not Found"
+        else:
+            return data["items"][0]["value"]["id"]
+    else:
+        return 400
     
+
+
+mpID = searchMP("cf143uu")
+
