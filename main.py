aaddfe568734e43f59cbc81b321d85521dbe7758
Toby Nott <NottTJ@cardiff.ac.uk>
2022-01-19 18:51:25 +0000

    added mpdetails function

--- main.py
+++ main.py
@@ -1,27 +1,6 @@
 import requests
 
 
-
-#if __name__ == "__main__":
-if False:
-    url = "https://members-api.parliament.uk/api/Location/Constituency/Search?searchText="+ search
-    search = str(input("Enter PostCode or other search term: "))
-    resp = requests.get(url)
-
-    print("Searching")
-
-    if resp.status_code == 200: 
-        print("Done!")
-        data = resp.json()
-        if data["totalResults"] == 0:
-            print("No results found")
-        print(data["resultContext"])
-        firstRes = (data["items"][0]["value"])
-        mp = firstRes["currentRepresentation"]["member"]["value"]
-        print("Your constituancy is {}".format(firstRes["name"]))
-        print("Your MP is {}".format(mp["nameDisplayAs"]))
-        print("Your MP belongs to the {} party".format(mp["latestParty"]["name"]))
-
 def searchMP(query): #searches for mp and returns ID
     url = "https://members-api.parliament.uk/api/Location/Constituency/Search?searchText="+ query
     resp = requests.get(url)
@@ -30,11 +9,25 @@ def searchMP(query): #searches for mp and returns ID
         if data["totalResults"] == 0:
             return "Not Found"
         else:
-            return data["items"][0]["value"]["id"]
+            return str(data["items"][0]["value"]["currentRepresentation"]["member"]["value"]["id"])
     else:
         return 400
     
+def mpDetails(mpID):
+    url = "https://members-api.parliament.uk/api/Members/" + mpID
+    resp = requests.get(url)
+    if resp.status_code == 200:
+        data = resp.json()
+        return data
+    else:
+        return "Not found"
+    
 
-
-mpID = searchMP("cf143uu")
-
+if __name__ == "__main__":
+    search = str(input("Enter PostCode or other search term: "))
+    searchMP(search)
+    firstRes = (data["items"][0]["value"])
+    mp = firstRes["currentRepresentation"]["member"]["value"]
+    print("Your constituancy is {}".format(firstRes["name"]))
+    print("Your MP is {}".format(mp["nameDisplayAs"]))
+    print("Your MP belongs to the {} party".format(mp["latestParty"]["name"]))
